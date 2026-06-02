218e944b82bb641679aa7cec5f261d75db113950
IT.bulka <i.it.bulka@gmail.com>
2026-06-02 16:10:24 +0200

    fix(db): increase MongoDB server selection and socket timeouts

--- app/db/connectDB.js
+++ app/db/connectDB.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const connectDB = async () => {
   await mongoose.connect(process.env.MONGODB_URL, {
-    dbName: process.env.MONGODB_DB_NAME
+    dbName: process.env.MONGODB_DB_NAME,
+    serverSelectionTimeoutMS: 60000,
+    socketTimeoutMS: 60000,
   })
 }
 
