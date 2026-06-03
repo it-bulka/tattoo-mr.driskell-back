9479bfe54ce260ad7d2c9a53af03069fe266a2fc
IT.bulka <i.it.bulka@gmail.com>
2026-06-03 10:21:22 +0200

    feat(brands): extend Brand model with slug, imgUrl, defaultLang fields

--- app/models/brands.js
+++ app/models/brands.js
@@ -1,14 +1,43 @@
 const mongoose = require('mongoose')
+const { langs } = require('../utils')
+const { BrandTranslation } = require('./brand-translation')
 
 const brandSchema = new mongoose.Schema({
-  name: {
+  slug: {
     type: String,
-    required: true
+    required: true,
+    unique: true
+  },
+  imgUrl: {
+    type: String,
+    default: ''
+  },
+  defaultLang: {
+    type: String,
+    enum: langs,
+    default: 'en'
+  }
+})
+
+brandSchema.pre('deleteOne', { document: true, query: false }, async function (next) {
+  try {
+    await BrandTranslation.deleteMany({ brandId: this._id })
+    next()
+  } catch (err) {
+    next(err)
+  }
+})
+
+brandSchema.pre('deleteMany', async function (next) {
+  try {
+    const docs = await this.model.find(this.getQuery())
+    await Promise.all(docs.map(doc => BrandTranslation.deleteMany({ brandId: doc._id })))
+    next()
+  } catch (err) {
+    next(err)
   }
 })
 
 const Brand = mongoose.model('Brand', brandSchema)
 
-module.exports = {
-  Brand
-}
+module.exports = { Brand }
